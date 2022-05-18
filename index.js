e1c912523e8a98b95f5faff6f09a01f88bddb4e7
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-19 04:58:33 +0600

    add completed status to the task: put route

--- index.js
+++ index.js
@@ -46,6 +46,17 @@ const run = async () => {
       return res.send(tasks);
     });
 
+    // completed task
+    app.put("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: { status: "completed" },
+      };
+      const result = await tasksCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // delete
     app.delete("/tasks/:id", async (req, res) => {
       const id = req.params.id;
