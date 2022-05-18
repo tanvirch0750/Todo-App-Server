2548027d0c07622ddaad234fabeed68592e693e5
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-18 20:40:08 +0600

    connect mongodb with database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const ObjectId = require("mongodb").ObjectId;
 
@@ -10,17 +11,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("todo app server is running");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r3o7j.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-// const run = async () => {
-//    try {
+const run = async () => {
+  try {
+    await client.connect();
+    console.log("todo app mongo db server is running");
 
-//    } finally {
-//    }
-//  };
-//  run().catch(console.dir);
+    // Main route
+    app.get("/", (req, res) => {
+      res.send("todo app server is running");
+    });
+  } finally {
+  }
+};
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Listening to car repair port", port);
